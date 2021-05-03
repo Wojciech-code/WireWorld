751e5eb4d9c270001bb3da7c50a4719ef6ed1afb
Kamil Smołecki <ksmolecki123@wp.pl>
2021-05-03 19:49:53 +0200

    Uzupełnienie bramek logicznych

---
+++ src/bramki/logiczne/XorGate.java
@@ -0,0 +1,49 @@
+package bramki.logiczne;
+
+import silnik.Cell;
+import silnik.Map;
+
+public class XorGate implements Gate{
+
+
+    @Override
+    public void setGate(Map m, int row, int column) {
+
+        for(Cell c : m){
+
+            if(( c.row == row && c.column == column) ||
+                    ( c.row == row && c.column == column+1) ||
+                    ( c.row == row && c.column == column+2) ||
+
+                    ( c.row == row+1 && c.column == column+3) ||
+
+                    ( c.row == row+2 && c.column == column+2) ||
+                    ( c.row == row+2 && c.column == column+3) ||
+                    ( c.row == row+2 && c.column == column+4) ||
+                    ( c.row == row+2 && c.column == column+5) ||
+
+                    ( c.row == row+3 && c.column == column+2) ||
+
+                    ( c.row == row+3 && c.column == column+5) ||
+                    ( c.row == row+3 && c.column == column+6) ||
+                    ( c.row == row+3 && c.column == column+7) ||
+                    ( c.row == row+3 && c.column == column+8) ||
+
+                    ( c.row == row+4 && c.column == column+2) ||
+                    ( c.row == row+4 && c.column == column+3) ||
+                    ( c.row == row+4 && c.column == column+4) ||
+                    ( c.row == row+4 && c.column == column+5) ||
+
+                    ( c.row == row+5 && c.column == column+3) ||
+
+                    ( c.row == row+6 && c.column == column) ||
+                    ( c.row == row+6 && c.column == column+1) ||
+                    ( c.row == row+6 && c.column == column+2))
+
+            {
+                c.setConductor();
+            }
+
+        }
+    }
+}
